6d7e153ee4babdb3bc915e828d42b4a7c97c94f7
lartl <luis.arce22@gmail.com>
2018-08-20 21:47:04 -0400

    Complete calendar8 functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,44 +9,19 @@ import { Month8Service } from './services/month8.service'
 export class AppComponent {
   months = [];
   startDateIn: string;
-  nroDaysIn = 0;
-  monthNames = {"US": ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ], "ES": ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-    "Julio", "Augosto", "Septiembre", "Octubre", "Noviembre", "Deciembre"
-  ]};
+  nroDaysIn = 1;
+
+  daysNamesI18N = [];
+
+  countryCode = "US";
+
   constructor(private monthSvc: Month8Service) {
     this.startDateIn = "";
-  }30
+    this.daysNamesI18N = [];
+  }
 
   generateCalendar8() {
-    let dateSplitted = this.startDateIn.split('-').map(sp => Number(sp));
-    let startDate = new Date(dateSplitted[0], dateSplitted[1] - 1, dateSplitted[2], 0, 0, 0); 
-    let aDay = 86400000;
-    let lastDate = new Date(dateSplitted[0], dateSplitted[1] - 1, dateSplitted[2], 0, 0, 0); 
-    lastDate = new Date(lastDate.setTime(lastDate.getTime() + this.nroDaysIn * aDay ));
-    
-    let startMonth = startDate.getMonth();
-    let lastMonth = lastDate.getMonth();
-    let startYear = startDate.getFullYear();
-    let lastYear = lastDate.getFullYear();
-
-    let i = startMonth;
-    let startDateAux = startDate;
-    this.months = [];
-
-    while ((i <= lastMonth && startYear == lastYear) || startDateAux.getTime() < lastDate.getTime()) {
-      let currentYear = startDateAux.getFullYear();
-      const { nextStartDay, monthResult } = this.monthSvc.generateMonthMatrix(startDateAux, lastDate, i);
-      this.months.push({
-        monthResult,
-        monthName: this.monthNames["US"][i],
-        currentYear
-      });
-      startDateAux = nextStartDay;
-      i++;
-      i = i % 12;
-      startYear = startDateAux.getFullYear();
-    }
+    this.months = this.monthSvc.generateCalendar8(this.startDateIn, this.nroDaysIn, this.countryCode);
+    this.daysNamesI18N = this.monthSvc.dayNames[this.countryCode];
   }
 }
--- src/app/services/month8.service.ts
+++ src/app/services/month8.service.ts
@@ -5,6 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class Month8Service {
 
+  monthNames = {"US": ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ], "ES": ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Augosto", "Septiembre", "Octubre", "Noviembre", "Deciembre"
+  ]};
+
+  dayNames = {"US": [ "S", "M", "T", "W", "T", "F", "S" ], 
+              "ES": [ "D", "L", "M", "M", "J", "V", "S" ]};
+
   constructor() { }
 
   generateMonthMatrix(start: Date, end: Date, month: number) {
@@ -21,5 +30,51 @@ export class Month8Service {
     }
 
     return { nextStartDay:startAux, monthResult: result};
+  }
+
+  generateCalendar8(startDateIn: string, nroDaysIn: number, countryCode: string, ) {
+
+    // generation crash with values more than 400
+    if (nroDaysIn > 400) [];
+
+    const aDay = 86400000;
+    let dateSplitted = startDateIn.split('-').map(sp => Number(sp));
+
+    // init start date and end date to generate calendar
+    let startDate = new Date(dateSplitted[0], dateSplitted[1] - 1, dateSplitted[2], 0, 0, 0); 
+    let lastDate = new Date(dateSplitted[0], dateSplitted[1] - 1, dateSplitted[2], 0, 0, 0); 
+    lastDate = new Date(lastDate.setTime(lastDate.getTime() + nroDaysIn * aDay ));
+    
+    // start and end month for the algorithm that generated the values
+    let startMonth = startDate.getMonth();
+    let lastMonth = lastDate.getMonth();
+    let startYear = startDate.getFullYear();
+    let lastYear = lastDate.getFullYear();
+
+    let i = startMonth;
+    let startDateAux = startDate;
+    let monthNamesI18N = this.monthNames[countryCode];
+    let months = [];
+
+    // loop that generates the matrix for each month
+    while ((i <= lastMonth && startYear == lastYear) || startDateAux.getTime() < lastDate.getTime()) {
+      let currentYear = startDateAux.getFullYear();
+      
+      // generates a matrix for a month
+      const { nextStartDay, monthResult } = this.generateMonthMatrix(startDateAux, lastDate, i);
+
+      // insert in the months arrays to display later with the component month8
+      months.push({
+        monthResult,
+        monthName: monthNamesI18N[i],
+        currentYear
+      });
+      startDateAux = nextStartDay;
+      i++;
+      i = i % 12;
+      startYear = startDateAux.getFullYear();
+    }
+
+    return months;
   }
 }
